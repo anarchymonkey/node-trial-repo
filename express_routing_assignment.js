28ce3e2eec4ba277b3a4dedcbaa8101049d76c7d
Aniket-Batabyal <Aniketbatabyal1996@gmail.com>
2018-08-15 12:32:56 +0530

    added repeat loops

--- express_routing_assignment.js
+++ express_routing_assignment.js
@@ -23,6 +23,17 @@ var name = req.params.name;
     res.send("YOU AREE WRONGGGGGGGGGGGGGGGGGGG!!");
   }
 });
+app.get("/repeat/:repeatThis/:no_of_times",function(req,res){
+    var repeatWhat = req.params.repeatThis;
+    var times = parseInt(req.params.no_of_times);
+    var result = ""; // an empty string to store
+
+    for(var i = 0; i < times ; i++)
+    {
+      result += repeatWhat + "\n"; // store the loops in a result
+    }
+      res.send(result);
+});
 
 app.listen(3000,function(){
   console.log(" THE SERVER HAS STARTED ");
