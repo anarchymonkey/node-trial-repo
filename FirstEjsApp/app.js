a5064ab75fbaf567435b2fd56b59d85d14b843d0
Aniket-Batabyal <Aniketbatabyal1996@gmail.com>
2018-08-16 22:28:17 +0530

    added posts page

--- FirstEjsApp/app.js
+++ FirstEjsApp/app.js
@@ -5,6 +5,20 @@ app.get("/testing/:name",function(req,res){
   var name = req.params.name;
   res.render("index.ejs",{nameVar : name });
 });
+
+app.get("/posts",function(req,res){
+      var posts = [
+                  {
+                      title:" hey my name is aniket",
+                      author : "anarchymonkey"},
+                  {
+                      title: " hey my name is nvKepler",
+                      author:"Neeelesh Vashist"
+                  }
+                ];
+                res.render("posts.ejs", {posts : posts})
+
+})
 app.listen(3000,function()
 {
   console.log("server has started");
--- app.js
+++ app.js
@@ -10,5 +10,21 @@ app.get("/nigga",function(req,res){
   res.send("I LOVE YOU <3");
 })
 
+app.get("/:name/comments/:number",function(req,res){
+  console.log("requested");
+
+  var name = req.params.name;
+  var number = req.params.number;
+
+  if(name.toLowerCase() == 'aniket')
+  {
+    res.send("flow pointers of request");
+  }
+  else{
+    res.send("flow pointers of request in the else section");
+  }
+})
+
+
 
 app.listen(3000);
