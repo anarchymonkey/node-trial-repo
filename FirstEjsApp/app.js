a9d9711120cfe109799a1e0aa29b897a16f6e655
Aniket-Batabyal <Aniketbatabyal1996@gmail.com>
2018-08-17 09:07:37 +0530

    added partials and css trial temp

--- FirstEjsApp/app.js
+++ FirstEjsApp/app.js
@@ -1,11 +1,11 @@
 var express = require("express"); // require express package
 var app = express(); // return value from express
 app.use(express.static("public")); // to use the folder public for css and javascript
-
+app.set("view engine","ejs"); // this will look for ejs files only otherwise if specified
 app.get("/testing/:name",function(req,res){
   var name = req.params.name;
   //res.render helps us interact with the ejs file
-  res.render("index.ejs",{nameVar : name }); // passing variable name as nameVar to the ejs file
+  res.render("index",{nameVar : name }); // passing variable name as nameVar to the ejs file
 });
 app.get("/posts",function(req,res){
   //array of posts
@@ -19,7 +19,7 @@ app.get("/posts",function(req,res){
                   }
                 ];
                 //we can loop through to get posts
-                res.render("posts.ejs", {posts : posts}); // rendering the posts var[array] as posts
+                res.render("posts", {posts : posts}); // rendering the posts var[array] as posts
 
 })
 
