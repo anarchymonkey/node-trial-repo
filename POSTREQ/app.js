312b579914a6aab65d1ea84f39ce449d08fd9588
Aniket-Batabyal <Aniketbatabyal1996@gmail.com>
2018-08-18 11:03:22 +0530

    added new posting mech in /posts

--- POSTREQ/app.js
+++ POSTREQ/app.js
@@ -1,11 +1,21 @@
 var express = require("express");
 var app = express();
+var bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine","ejs");
+var friends = ["Aniket","Anil","Jande","Ankush","Nidhi"];
 app.get("/",function(request,response){
   response.render("index");
 });
+
+app.post("/addfriend",function(req,res){
+var newFriend = req.body.newFriend;
+  friends.push(newFriend);
+  //res.send("YOU HAVE REACHED POST ROUTE") // getting data
+  res.redirect("/posts");
+});
 app.get("/posts",function(request,response){
-  var friends = ["Aniket","Anil","Jande","Ankush","Nidhi"];
+
   response.render("friendposts",{friends : friends});
 });
 app.listen(3000,function(){
