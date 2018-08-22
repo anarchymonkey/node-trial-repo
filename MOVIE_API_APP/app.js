2eaf5da9bf1ce3c44aa949fb12ed01c16f33cafa
Aniket-Batabyal <Aniketbatabyal1996@gmail.com>
2018-08-22 15:32:03 +0530

    added minor changes

--- MOVIE_API_APP/app.js
+++ MOVIE_API_APP/app.js
@@ -3,21 +3,33 @@ var bodyParser = require('body-parser');
 var app = express();
 var request = require('request'); // request package
 var time;
+var city;
+var country;
+var region;
 app.set("view engine","ejs");
 app.get('/',function(req,res){
   console.log('THE HOME PAGE IS ACCESSED');
   res.render("index");
 });
+// request for the hawai api time
+request("https://query.yahooapis.com/v1/public/yql?q=select%20item.condition%20from%20weather.forecast%20where%20woeid%20%3D%202487889&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys",function(err,res,body){
+
+  var parsedJSON = JSON.parse(body);
+  time = parsedJSON["query"]["results"]["channel"]["item"]["condition"]["date"];
+
+});
+
+// request for the other time
 request("https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22nome%2C%20ak%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys",function(err,res,body){
 var parsedJSON = JSON.parse(body);
     // time = parsedJSON["query"]["results"]['channel']['item']['condition']['date'];
     //console.log(time);
     console.log("THE CITY IS...");
-    console.log(parsedJSON["query"]["results"]["channel"]["location"]["city"]);
+    city =parsedJSON["query"]["results"]["channel"]["location"]["city"];
     console.log("THE COUNTRY IS...");
-    console.log(parsedJSON["query"]["results"]["channel"]["location"]["country"]);
+    country = parsedJSON["query"]["results"]["channel"]["location"]["country"];
     console.log("THE REGION IS SITUATED IN..");
-    console.log(parsedJSON["query"]["results"]["channel"]["location"]["region"]);
+    region = parsedJSON["query"]["results"]["channel"]["location"]["region"];
 
     // parse the json file and put it in a object
 
@@ -29,7 +41,10 @@ app.get('/:name/:id',function(req,res){
   res.render("apiDisplay",{
                             time : time,
                             name : name,
-                            id : id
+                            id : id,
+                            city : city,
+                            country : country,
+                            region : region
                           });
                         });
 app.listen(3000,function(){
